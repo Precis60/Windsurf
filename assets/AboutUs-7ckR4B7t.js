db6ba29daa37076c7a5c386d7837f64a8dff6ae0
Precis60 <jamie@precisioncabling.onmicrosoft.com>
2025-09-21 00:57:54 +1000

    Updates

--- assets/AboutUs-7ckR4B7t.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BJHBaGN9.js";import"./vendor-DR4o8xae.js";const s=()=>e.jsxs("div",{className:"page-content",children:[e.jsx("h1",{children:"About Us"}),e.jsx("p",{children:"Learn more about Precision Cabling & Automation and our experienced team."})]});export{s as default};
--- assets/Consultation-I4ZvSMwb.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BJHBaGN9.js";import"./vendor-DR4o8xae.js";const s=()=>o.jsxs("div",{className:"page-content",children:[o.jsx("h1",{children:"Consultation"}),o.jsx("p",{children:"Book a consultation with our technology experts for your next project."})]});export{s as default};
