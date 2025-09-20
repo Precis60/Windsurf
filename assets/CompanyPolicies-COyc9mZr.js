b80c8da7bd10fd8600bf24ebf02680e22030d621
Precis60 <jamie@precisioncabling.onmicrosoft.com>
2025-09-21 01:38:29 +1000

    Updates

--- assets/CompanyPolicies-COyc9mZr.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-4rICCF5Y.js";import"./vendor-DR4o8xae.js";const i=()=>o.jsxs("div",{className:"page-content",children:[o.jsx("h1",{children:"Company Policies"}),o.jsx("p",{children:"Read about our company policies and commitment to quality and safety."})]});export{i as default};
