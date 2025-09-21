899e4687864932e3e0f3fbbef8f7a8939532a822
Precis60 <Precis60@users.noreply.github.com>
2025-09-21 10:01:00 +0000

    deploy: d790847abbd49942abeae75bf5d85618f1637867

---
+++ assets/Services-VEcZpivM.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CuO5F8ER.js";import"./vendor-DPLMwiL-.js";const o=()=>e.jsxs("div",{className:"page-content",children:[e.jsx("h1",{children:"Our Services"}),e.jsx("p",{children:"Explore our wide range of cabling and automation services tailored to your needs."})]});export{o as default};
