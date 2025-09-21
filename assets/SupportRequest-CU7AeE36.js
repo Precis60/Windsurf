3fb47ee901e1977bd7a693b437ebe8784b1105e1
Precis60 <Precis60@users.noreply.github.com>
2025-09-21 03:07:09 +0000

    deploy: 578a0e9a57d9334054411e525d4a75381d36f1d0

--- assets/SupportRequest-CU7AeE36.js
+++ assets/SupportRequest-CU7AeE36.js
@@ -1,4 +1,4 @@
-import{j as e,s as m}from"./index-BrwF-OIQ.js";import{b as n}from"./vendor-DR4o8xae.js";const y=()=>{const[i,l]=n.useState({name:"",email:"",phone:"",company:"",subject:"",description:"",priority:"medium",category:"general"}),[t,d]=n.useState(!1),[x,p]=n.useState(!1),[c,s]=n.useState(""),r=o=>{l({...i,[o.target.name]:o.target.value}),s("")},u=async o=>{o.preventDefault(),d(!0),s("");try{const a={subject:`[${i.name}] ${i.subject}`,description:`
+import{j as e,s as m}from"./index-DpNr_0ZG.js";import{b as n}from"./vendor-DR4o8xae.js";const y=()=>{const[i,l]=n.useState({name:"",email:"",phone:"",company:"",subject:"",description:"",priority:"medium",category:"general"}),[t,d]=n.useState(!1),[x,p]=n.useState(!1),[c,s]=n.useState(""),r=o=>{l({...i,[o.target.name]:o.target.value}),s("")},u=async o=>{o.preventDefault(),d(!0),s("");try{const a={subject:`[${i.name}] ${i.subject}`,description:`
 Contact Information:
 - Name: ${i.name}
 - Email: ${i.email}
