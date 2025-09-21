482ba8244caa30d4b9dc6bbd75cbaae1f49c3d0a
Precis60 <Precis60@users.noreply.github.com>
2025-09-21 07:09:02 +0000

    deploy: 28261afab49fb9ca4c5d9bbd6e6a901e73784548

--- assets/CompanyPolicies-Cs7H4TBK.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-NaBvualv.js";import"./vendor-DPLMwiL-.js";const i=()=>o.jsxs("div",{className:"page-content",children:[o.jsx("h1",{children:"Company Policies"}),o.jsx("p",{children:"Read about our company policies and commitment to quality and safety."})]});export{i as default};
