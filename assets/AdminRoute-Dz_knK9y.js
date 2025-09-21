3d08799de79addcb34aecb78950a41020910699a
Precis60 <Precis60@users.noreply.github.com>
2025-09-21 07:45:29 +0000

    deploy: 03911000bbc74bfb13e6b817c6bdc07fab755d90

---
+++ assets/AdminRoute-Dz_knK9y.js
@@ -0,0 +1,1 @@
+import{a as s,j as t}from"./index-Bi7HTg4J.js";import{N as i,O as a}from"./vendor-DPLMwiL-.js";const u=()=>{const e=s.isAuthenticated(),o=s.isAdmin();return e?o?t.jsx(a,{}):t.jsx(i,{to:"/portal"}):t.jsx(i,{to:"/login"})};export{u as default};
--- assets/NotFound-Djxbw5Tl.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CtGbqMWx.js";import"./vendor-DPLMwiL-.js";const t=()=>o.jsxs("div",{className:"page-content",children:[o.jsx("h1",{children:"404 - Page Not Found"}),o.jsx("p",{children:"The page you are looking for does not exist."})]});export{t as default};
