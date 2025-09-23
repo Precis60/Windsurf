b9c39af7ba8f8cf4d60ee096ad4de98144b03401
Precis60 <Precis60@users.noreply.github.com>
2025-09-23 12:49:48 +0000

    deploy: 102b3e4dd2765e12f658237469f954a9b4ac8102

---
+++ assets/NotFound-UOYr74EY.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BcWSzOQo.js";import"./vendor-DPLMwiL-.js";const t=()=>o.jsxs("div",{className:"page-content",children:[o.jsx("h1",{children:"404 - Page Not Found"}),o.jsx("p",{children:"The page you are looking for does not exist."})]});export{t as default};
