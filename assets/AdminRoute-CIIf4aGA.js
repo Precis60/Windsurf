707b039434fd826409136143a8d56404024c1042
Precis60 <jamie@precisioncabling.onmicrosoft.com>
2025-11-08 19:23:23 +1100

    Updates

--- assets/AdminRoute-CIIf4aGA.js
+++
@@ -1,1 +0,0 @@
-import{a as s,j as t}from"./index-Bq6MzYro.js";import{N as i,O as a}from"./vendor-Dd4J3GVN.js";const u=()=>{const e=s.isAuthenticated(),o=s.isAdmin();return e?o?t.jsx(a,{}):t.jsx(i,{to:"/portal"}):t.jsx(i,{to:"/login"})};export{u as default};
