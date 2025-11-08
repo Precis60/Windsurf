71a3f8456233eb6e83bd41b4391f5bdb0b45cec8
Precis60 <Precis60@users.noreply.github.com>
2025-11-08 11:17:27 +0000

    deploy: 003e5fbc66d18ffc4c92877af61bd2d1479164cb

--- assets/AboutUs-BJpF4UyD.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BjU6Wv_-.js";import"./vendor-Dd4J3GVN.js";const s=()=>e.jsxs("div",{className:"page-content",children:[e.jsx("h1",{children:"About Us"}),e.jsx("p",{children:"Learn more about Precision Cabling & Automation and our experienced team."})]});export{s as default};
