f3a146b7a6462adf36b2c16036c3eb5d6748df06
Precis60 <Precis60@users.noreply.github.com>
2025-11-08 08:33:04 +0000

    deploy: 7cd29366e15bc695fa2df167b0047689ab09040c

--- assets/Services-BSbXW0lj.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CS7WQ_XK.js";import"./vendor-Dd4J3GVN.js";const o=()=>e.jsxs("div",{className:"page-content",children:[e.jsx("h1",{children:"Our Services"}),e.jsx("p",{children:"Explore our wide range of cabling and automation services tailored to your needs."})]});export{o as default};
---
+++ assets/Services-lciLzdi6.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-B-vy2uDm.js";import"./vendor-Dd4J3GVN.js";const o=()=>e.jsxs("div",{className:"page-content",children:[e.jsx("h1",{children:"Our Services"}),e.jsx("p",{children:"Explore our wide range of cabling and automation services tailored to your needs."})]});export{o as default};
